d50ad5c227e5dd23124aa8cebe1dddd0ec7842de
mark <mark_c@naver.com>
2023-09-22 17:45:28 +0900

    230922

---
+++ 프로그래머스/덧셈식 출력하기.py
@@ -0,0 +1,5 @@
+a, b = map(int, input().strip().split(' '))
+print(a, '+', b, '=', a + b)
+
+# f스트링이 되네
+print(f"{a} + {b} = {a + b}")
---
+++ 프로그래머스/문자열 붙여서 출력하기.py
@@ -0,0 +1,5 @@
+str1, str2 = input().strip().split(' ')
+print(str1 + str2)
+
+#흠, 구분표시 없음으로 붙인다.
+print(str1, str2, sep='')
